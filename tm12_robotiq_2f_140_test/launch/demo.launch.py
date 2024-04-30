6a180c5afbffdd72e5083a2cfab50b490b36b6dc
Nemwel <nemwel.keoro@gmail.com>
2024-04-30 13:49:01 +0100

    Update demo.launch.py

--- tm12_robotiq_2f_140_test/launch/demo.launch.py
+++ tm12_robotiq_2f_140_test/launch/demo.launch.py
@@ -1,49 +1,50 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node 
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import LaunchConfiguration
 from moveit_configs_utils import MoveItConfigsBuilder
 from launch_ros.parameter_descriptions import ParameterValue
-from launch_param_builder import ParameterBuilder, load_yaml, load_xacro
-import xacro
-
-from ament_index_python import get_package_share_directory
 
 from moveit_configs_utils.launch_utils import (
     add_debuggable_node,
     DeclareBooleanLaunchArg,
 )
-from launch.actions import (
-    DeclareLaunchArgument,
-    IncludeLaunchDescription,
-)
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
 
     ld = LaunchDescription()
     ld.add_action(DeclareBooleanLaunchArg("use_rviz", default_value=True))
     ld.add_action(DeclareLaunchArgument("publish_frequency", default_value="15.0"))
-    model_launch_arg = DeclareLaunchArgument(
-        name="model",
-        default_value=os.path.join(
-            get_package_share_directory("tm12_robotiq_2f_140_moveit_config"),
-            "config/tm12.urdf.xacro"
+    
+    # MoveGroup parameters
+    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
+    ld.add_action(
+        DeclareBooleanLaunchArg("allow_trajectory_execution", default_value=True)
+    )
+    ld.add_action(
+        DeclareBooleanLaunchArg("publish_monitored_planning_scene", default_value=True)
+    )
+    '''
+    # load non-default MoveGroup capabilities (space separated)
+    ld.add_action(
+        DeclareLaunchArgument(
+            "capabilities",
+            default_value=moveit_config.move_group_capabilities["capabilities"],
         )
     )
-    ld.add_action(model_launch_arg)
+    # inhibit these default MoveGroup capabilities (space separated)
+    ld.add_action(DeclareLaunchArgument("disable_capabilities", default_value=""))
+    '''
+
+    # do not copy dynamics information from /joint_states to internal robot monitoring
+    # default to false, because almost nothing in move_group relies on this information
+    ld.add_action(DeclareBooleanLaunchArg("monitor_dynamics", default_value=False))
+
+    should_publish = LaunchConfiguration("publish_monitored_planning_scene")
+
 
     moveit_config = MoveItConfigsBuilder("tm12", package_name="tm12_robotiq_2f_140_moveit_config").to_moveit_configs()
-    
-    robot_description = ParameterValue(
-        Command(
-            [
-                'xacro ',
-                LaunchConfiguration('model'),
-            ]
-        ),
-        value_type=str
-    )
-    #robot_description = load_xacro(LaunchConfiguration('model')) 
 
     # broadcast static tf of virtual_joints
     ld.add_action(
@@ -61,18 +62,100 @@ def generate_launch_description():
         )
     )
     # Given the published joint states, publish tf for the robot links
-    rsp_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        respawn=True,
-        output="screen",
-        parameters=[
-            moveit_config.robot_description, #robot_description, #
-            {
-                "publish_frequency": LaunchConfiguration("publish_frequency"),
-            },
-        ],
+    ld.add_action(
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            respawn=True,
+            output="screen",
+            parameters=[
+                moveit_config.robot_description, #robot_description, #
+                {
+                    "publish_frequency": LaunchConfiguration("publish_frequency"),
+                },
+            ],
+        )
     )
-    ld.add_action(rsp_node)
+
+    # MoveGroup settings
+    move_group_configuration = {
+        "publish_robot_description_semantic": True,
+        "allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
+        # Note: Wrapping the following values is necessary so that the parameter value can be the empty string
+        #"capabilities": ParameterValue(LaunchConfiguration("capabilities"), value_type=str),
+        #"disable_capabilities": ParameterValue(LaunchConfiguration("disable_capabilities"), value_type=str),
+        # Publish the planning scene of the physical robot so that rviz plugin can know actual robot
+        "publish_planning_scene": should_publish,
+        "publish_geometry_updates": should_publish,
+        "publish_state_updates": should_publish,
+        "publish_transforms_updates": should_publish,
+        "monitor_dynamics": False,
+    }
+    move_group_params = [
+        moveit_config.to_dict(),
+        move_group_configuration,
+    ]
+
+    add_debuggable_node(
+        ld,
+        package="moveit_ros_move_group",
+        executable="move_group",
+        commands_file=str(moveit_config.package_path / "launch" / "gdb_settings.gdb"),
+        output="screen",
+        parameters=move_group_params,
+        extra_debug_args=["--debug"],
+        # Set the display variable, in case OpenGL code is used internally
+        additional_env={"DISPLAY": os.environ["DISPLAY"]},
+    )
+
+    # Run Rviz and load the default config to see the state of the move_group node
+    ld.add_action(
+        DeclareLaunchArgument(
+            "rviz_config",
+            default_value=str(moveit_config.package_path / "config/moveit.rviz"),
+        )
+    )
+
+    rviz_parameters = [
+        moveit_config.planning_pipelines,
+        moveit_config.robot_description_kinematics,
+    ]
+
+    add_debuggable_node(
+        ld,
+        package="rviz2",
+        executable="rviz2",
+        output="log",
+        respawn=False,
+        arguments=["-d", LaunchConfiguration("rviz_config")],
+        parameters=rviz_parameters,
+    )
+
+    # Fake joint driver
+    ld.add_action(
+        Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            parameters=[
+                moveit_config.robot_description,
+                str(moveit_config.package_path / "config/ros2_controllers.yaml"),
+            ],
+        )
+    )
+
+    # Controllers
+    controller_names = moveit_config.trajectory_execution.get(
+        "moveit_simple_controller_manager", {}
+    ).get("controller_names", []) 
+    for controller in controller_names + ["joint_state_broadcaster"]:
+        ld.add_action(
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[controller],
+                output="screen",
+            )
+        )
+
 
     return ld
